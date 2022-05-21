0ecd7e9e9871346665a613573f5937ca5dad7963
satyadev0655 <103864602+satyadev0655@users.noreply.github.com>
2022-05-21 21:51:12 +0530

    Update 10 - Functions.js

--- 10 - Functions.js
+++ 10 - Functions.js
@@ -7,3 +7,61 @@ function getData(color) {
   }
 }
 getData('test');
+
+
+//Primitive types
+
+//---------------------------------------
+function hello(name) {
+  console.log(name);
+}
+hello('Alex');
+
+//---------------------------------------
+function sum(a, b) {
+  return a + b;
+}
+
+res1 = sum(1, 2);
+console.log(res1); //3
+
+//---------------------------------------
+//Anonymous function
+sumFun1 = function (a, b) {
+  return a + b;
+};
+
+console.log(sumFun1(3, 4)); //7
+
+//---------------------------------------
+//Arrow Function
+hello = (name) => {
+  console.log(name);
+};
+
+hello('Benn'); //Benn
+
+//remove {}
+hello1 = (name) => console.log(name);
+hello1('Kevin');
+
+hello3 = (p1, p2) => {
+  console.log(p1);
+};
+
+hello3('a', 'b');
+
+hello4 = (p1) => console.log(p1);
+
+hello5 = (p1) => {
+  console.log(p1);
+};
+
+hello4('A');
+
+hello6 = (p1) => {
+  console.log('hello 6');
+};
+
+consoel.log(hello6());
+
