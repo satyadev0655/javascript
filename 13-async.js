6bd2f8853bda2f7fe01870f602aa0bcef07cb551
satyadev0655 <103864602+satyadev0655@users.noreply.github.com>
2022-05-24 16:24:42 +0530

    Update 13-async.js

--- 13-async.js
+++ 13-async.js
@@ -10,3 +10,43 @@ setTimeout(function(){
     console.log('y');
 }, 1000);
 console.log('z');
+
+//Promoise
+let result = '0';
+
+const doSomething = (callback) => {
+  setTimeout(() => {
+     retult = 10;
+  }, 2000);
+  callback(result);
+};
+
+doSomething((result) => {
+  console.log(result);
+});
+
+//Output 0
+
+const doSomething = new Promise((resolve, reject) => 
+{ 
+    let success = true
+    if (success) { 
+        setTimeout(() => {
+         resolve(10)
+  }, 2000);
+        } 
+    else { 
+        reject('this error occurred') 
+    } 
+} )
+
+
+doSomething.then(
+    result => { 
+        console.log(result) 
+    }).catch(error => { 
+        console.log(error) 
+    })
+    
+//Output : 10    
+
