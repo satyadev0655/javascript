2f97cd703eacdf29bd1580ebb686edde77a3b95b
satyadev0655 <103864602+satyadev0655@users.noreply.github.com>
2022-05-25 06:27:41 +0530

    Create 13 - Async1.js

---
+++ 13 - Async1.js
@@ -0,0 +1,11 @@
+//Ex1
+function getBusDetails(busCode){
+    
+    let resObj= {k1: "v1", k2: "v2", k3 : "v3"};
+    return resObj;
+}
+
+
+let res = getBusDetails(1001);
+console.log(res["k1"]); //v1
+
